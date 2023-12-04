26bcdae378d96d04a87607ee2291ba34e09ff2cb
THEjacob1000 <jacob.s.sanderson@gmail.com>
2023-12-04 11:02:08 +1100

    Added Resend domain so it can work with any user

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -43,7 +43,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: "onboarding@resend.dev",
+        fromAddress: "digitalhippo@sanddev.net",
         fromName: "Digital Hippo",
       },
       secret: process.env.PAYLOAD_SECRET,
