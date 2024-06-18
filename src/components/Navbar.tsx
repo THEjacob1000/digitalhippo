a2a01db164bc45f16f7ba1f467ff5d28af912572
THEjacob1000 <jacob.s.sanderson@gmail.com>
2024-06-18 15:38:48 +1000

    build: fixed build error in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,9 @@ import MobileNav from "./MobileNav";
 
 const Navbar = async () => {
   const nextCookies = cookies();
-  const { user } = (await getServerSideUser(nextCookies)) ?? {};
+  const { user } = (await getServerSideUser(nextCookies)) ?? {
+    user: null,
+  };
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
